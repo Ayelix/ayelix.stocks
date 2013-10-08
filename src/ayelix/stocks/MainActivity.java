2d1fa76b78497d59eac090e8594b2c07b53df6d9
Alex Mosolgo <amosolgo@gmail.com>
2013-10-07 22:12:54 -0400

    Added very basic UI and listeners for search field submissions ("Go" keyboard key and onscreen Go button).

--- src/ayelix/stocks/MainActivity.java
+++ src/ayelix/stocks/MainActivity.java
@@ -1,15 +1,63 @@
 package ayelix.stocks;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
 public class MainActivity extends Activity {
+	private static final String TAG = "MainActivity";
+	
+	private EditText searchEditText;
+	private Button goButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		// Get the views
+		searchEditText = (EditText) findViewById(R.id.searchEditText);
+		goButton = (Button) findViewById(R.id.goButton);
+
+		// Create the listener for the search box "Go" action
+		searchEditText.setOnEditorActionListener(new OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				boolean handled = false;
+				// The GO action will start a search
+				if (EditorInfo.IME_ACTION_GO == actionId) {
+					search(searchEditText.getText().toString());
+					handled = true;
+				}
+				return handled;
+			}
+		});
+		
+		// Create the listener for the Go button
+		goButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				search(searchEditText.getText().toString());
+			}
+		});
+
+	}
+	
+	/**
+	 * Starts a search for the given string.
+	 * @param searchString The string to search for (stock symbol or company name).
+	 */
+	private void search(final String searchString) {
+		Log.d(TAG, "Searching string: " + searchString);
 	}
 
 	@Override
